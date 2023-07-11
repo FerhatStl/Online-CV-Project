dd089b5bca066ed21ac3d7454e0186c48e71565f
Ferhat Çelik <ferhatsteel34@gmail.com>
2023-07-11 15:38:17 +0300

    Upload
    Uploaded all files.

---
+++ WebApplication1/WebApplication1/Controllers/SkillController.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class SkillController : Controller
+    {
+        
+        Context db = new Context();
+
+        public ActionResult Index()
+        {
+            List<Skill> skills = db.Skills.ToList();
+            List<Language> languages = db.Languages.ToList();
+
+            var model = new Tuple<List<Skill>, List<Language>>(skills, languages);
+            return View(model);
+        }
+    }
+}
